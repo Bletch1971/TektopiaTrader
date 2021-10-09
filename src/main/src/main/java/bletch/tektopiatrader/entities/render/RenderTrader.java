fc980e3b2c2024a10e1c9d684d8138020bffbbe5
Brett Hewitson <bletch1971@hotmail.com>
2021-10-09 15:43:17 +1000

    Update RenderTrader.java

--- src/main/src/main/java/bletch/tektopiatrader/entities/render/RenderTrader.java
+++ src/main/src/main/java/bletch/tektopiatrader/entities/render/RenderTrader.java
@@ -1,7 +1,6 @@
 package bletch.tektopiatrader.entities.render;
 
 import bletch.tektopiatrader.entities.EntityTrader;
-import bletch.tektopiatrader.entities.EntityTrader_old;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
@@ -10,7 +9,7 @@ public class RenderTrader<T extends EntityTrader> extends RenderVillager<T> {
 	public static final RenderTrader.Factory<EntityTrader> FACTORY;
 	
 	public RenderTrader(RenderManager manager) {
-		super(manager, EntityTrader_old.MODEL_NAME, false, 128, 64, EntityTrader_old.MODEL_NAME);
+		super(manager, EntityTrader.MODEL_NAME, false, 128, 64, EntityTrader.MODEL_NAME);
 	}
 	
 	public static class Factory<T extends EntityTrader> implements IRenderFactory<T>
