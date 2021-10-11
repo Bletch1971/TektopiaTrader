8a0cbfa344b261219a68704c98cf5a2adb64277b
Brett Hewitson <bletch1971@hotmail.com>
2021-10-11 22:01:32 +1000

    version bump

--- src/main/src/main/java/bletch/tektopiatrader/core/ModDetails.java
+++ src/main/src/main/java/bletch/tektopiatrader/core/ModDetails.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 @ParametersAreNonnullByDefault
 public class ModDetails {
 	private static final int VersionMajor = 1;
-	private static final int VersionMinor = 0;
+	private static final int VersionMinor = 1;
 	private static final int VersionRevision = 0;
 
 	public static final String MOD_ID = "tektopiatrader";
