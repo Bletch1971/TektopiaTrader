b6b527c8e3c5f68ed502167f5ce2f8c158ee5a0c
Brett Hewitson <bletch1971@hotmail.com>
2021-11-20 11:09:40 +1000

    LoggerUtils Changes

--- src/main/src/main/java/bletch/tektopiatrader/utils/LoggerUtils.java
+++ src/main/src/main/java/bletch/tektopiatrader/utils/LoggerUtils.java
@@ -2,10 +2,13 @@ package bletch.tektopiatrader.utils;
 
 import bletch.common.utils.LoggerBase;
 import bletch.tektopiatrader.core.ModConfig;
+import bletch.tektopiatrader.core.ModDetails;
 
 public class LoggerUtils extends LoggerBase {
-
-	public static LoggerUtils instance = new LoggerUtils();
+	
+	public static void Initialise(String debugLogFile) {
+		LoggerBase.Initialise(new LoggerUtils(), ModDetails.MOD_NAME, debugLogFile);
+	}
 	
 	@Override
 	public void debug(String message, Boolean checkConfig) {
